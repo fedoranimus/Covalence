e811a080d1059ec67ba5e307764fce71b5cd4acc
Tim Turner <timdturner@gmail.com>
2017-05-21 07:17:33 -0400

    Updated startup

--- Covalence/Startup.cs
+++ Covalence/Startup.cs
@@ -1,13 +1,18 @@
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNet.Security.OpenIdConnect.Primitives;
+using Covalence.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 
 namespace Covalence
 {
@@ -28,8 +33,73 @@ namespace Covalence
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Enable CORS
+            services.AddCors();
+
             // Add framework services.
             services.AddMvc();
+
+            // Build connection string
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = "localhost";   // update me
+            builder.UserID = "sa";              // update me
+            builder.Password = "<YourStrong!Passw0rd>";      // update me
+            builder.InitialCatalog = "master";
+
+            //var connection = @"Server=(172.17.0.2)\mssqllocaldb;Database=Covalence;User Id=sa;Password=YourStrong!Passw0rd;";
+            // services.AddDbContext<ApplicationDbContext>(options => {
+            //     options.UseSqlServer(builder.ConnectionString);
+            //     options.UseOpenIddict();
+            // });
+
+            //For Development
+            services.AddDbContext<ApplicationDbContext>( options => {
+                options.UseInMemoryDatabase();
+                options.UseOpenIddict();
+            });
+
+            // Register the Identity services.
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.ClaimsIdentity.UserNameClaimType = OpenIdConnectConstants.Claims.Name;
+                options.ClaimsIdentity.UserIdClaimType = OpenIdConnectConstants.Claims.Subject;
+                options.ClaimsIdentity.RoleClaimType = OpenIdConnectConstants.Claims.Role;
+            });
+
+            // Register the OpenIddict services, including the default Entity Framework stores.
+            services.AddOpenIddict(options => 
+            {
+                // Register the Entity Framework stores.
+                options.AddEntityFrameworkCoreStores<ApplicationDbContext>();
+                // Register the ASP.NET Core MVC binder used by OpenIddict.
+                // Note: if you don't call this method, you won't be able to
+                // bind OpenIdConnectRequest or OpenIdConnectResponse parameters.
+                options.AddMvcBinders();
+
+                // Enable the token endpoint (required to use the password flow).
+                options.EnableTokenEndpoint("/connect/token");
+
+                // Allow client applications to use the grant_type=password flow.
+                options.AllowPasswordFlow();
+                options.AllowRefreshTokenFlow();
+
+                // Return a JWT rather than a traditional token
+                //.UseJsonWebTokens()
+
+                // During development, you can disable the HTTPS requirement.
+                options.DisableHttpsRequirement();
+
+                // Register a new ephemeral key, that is discarded when the application
+                // shuts down. Tokens signed using this key are automatically invalidated.
+                // This method should only be used during development.
+                options.AddEphemeralSigningKey();
+            });
+
+            services.AddScoped<ITagService, TagService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +107,22 @@ namespace Covalence
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
+
+            //TODO: Only see when in development!!!
+            //if(_env.IsDevelopment())
+                app.Seed();
+
+            app.UseCors(builder => 
+                builder.AllowAnyHeader()
+                       .AllowAnyMethod()
+                       .AllowAnyOrigin()
+            );
+
+            app.UseIdentity();
+
+            app.UseOAuthValidation();
+
+            app.UseOpenIddict();
 
             if (env.IsDevelopment())
             {
