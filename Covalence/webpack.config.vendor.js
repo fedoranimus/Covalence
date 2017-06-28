4b0f653161edfa4b181229dc772ce204b20a7083
Tim Turner <timdturner@gmail.com>
2017-06-28 17:42:42 -0400

    Fix build, update TS, replace Pure with Bulma

--- Covalence/webpack.config.vendor.js
+++ Covalence/webpack.config.vendor.js
@@ -33,7 +33,7 @@ module.exports = ({ prod } = {}) => {
                 'aurelia-templating-router',
                 'aurelia-api',
                 'aurelia-authentication',
-                'purecss'
+                'bulma'
             ],
         },
         output: {
