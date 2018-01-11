b1fb8585df8fd9189f960b77c78586be7ac68caf
Tim Turner <timdturner@gmail.com>
2018-01-11 17:30:57 -0500

    Fix connection update issue when the updated user is not on the current page of user results

--- Covalence/ClientApp/app/store/connectionActions.ts
+++ Covalence/ClientApp/app/store/connectionActions.ts
@@ -11,22 +11,27 @@ export async function updateConnection(state: State, userId: string, newConnecti
     try {
         const connections = await connectionApi(userId);
         const user = state.results.items.find(u => u.id == userId);
-        user.connectionStatus = newConnectionStatus;
-    
-        const index = state.results.items.findIndex(u => u.id == userId);
-    
-        return { ...state, ...{ 
-                results: {
-                    ...state.results,
-                    items: [
-                        ...state.results.items.slice(0, index),
-                        Object.assign({}, user),
-                        ...state.results.items.slice(index + 1)
-                    ]
-                },
-                connections: connections 
-            } 
-        };
+        if(user) {
+            user.connectionStatus = newConnectionStatus;
+        
+            const index = state.results.items.findIndex(u => u.id == userId);
+        
+            return { ...state, ...{ 
+                    results: {
+                        ...state.results,
+                        items: [
+                            ...state.results.items.slice(0, index),
+                            Object.assign({}, user),
+                            ...state.results.items.slice(index + 1)
+                        ]
+                    },
+                    connections: connections 
+                } 
+            };
+        } else {
+            return { ...state, ...{ connections }};
+        }
+        
     } catch(e) {
         console.error(e);
         return { ...state };
