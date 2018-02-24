a583cbeec11f57b96d096cbed4b679daaf141a57
Tim Turner <timdturner@gmail.com>
2018-02-24 16:47:01 -0500

    fix(search): Reorganize search-bar, fix up css

--- Covalence/ClientApp/app/pages/app-home/app-home.ts
+++ Covalence/ClientApp/app/pages/app-home/app-home.ts
@@ -11,22 +11,16 @@ import { SearchService } from 'services/searchService';
 import { Store } from 'aurelia-store';
 import { State } from 'store/state';
 import { getCurrentUser } from 'store/userActions';
-import { search } from 'store/searchActions';
 
 @autoinject
 export class AppHome {
     private state: State;
     // TODO - create subscription array 
 
-    constructor(private authService: AuthService, private router: Router, private searchService: SearchService, private connectionService: ConnectionService, private store: Store<State>) {
+    constructor(private authService: AuthService, private router: Router, private connectionService: ConnectionService, private store: Store<State>) {
         store.state.subscribe(state => {
             this.state = state;
         });
-    }
-
-    async search() {
-        const searchQuery = this.state.searchQuery;
-        this.store.dispatch(search, searchQuery, null, (searchQuery) => this.searchService.getResults(searchQuery, null));
     }
 
     @computedFrom('authService.authenticated')
--- Covalence/ClientApp/app/resources/custom-elements/search-bar/search-bar.ts
+++ Covalence/ClientApp/app/resources/custom-elements/search-bar/search-bar.ts
@@ -1,13 +1,18 @@
 import { bindable, autoinject } from "aurelia-framework";
 import { State } from "store/state";
 import { Store } from "aurelia-store";
-import { addSearchParam, removeSearchParam, clearSearchParams } from "store/searchActions";
+import { addSearchParam, removeSearchParam, clearSearchParams, search } from "store/searchActions";
+import { SearchService } from "services/searchService";
 
 @autoinject
 export class SearchBar {
     @bindable searchQuery;
-    constructor(private store: Store<State>) {
+    private state: State;
 
+    constructor(private store: Store<State>, private searchService: SearchService) {
+        store.state.subscribe(state => {
+            this.state = state;
+        });
     }
 
     bind() {
@@ -19,5 +24,10 @@ export class SearchBar {
             this.store.dispatch(addSearchParam, tagName);
         else
             this.store.dispatch(removeSearchParam, tagName);
+    }
+
+    async search() {
+        const searchQuery = this.state.searchQuery;
+        this.store.dispatch(search, searchQuery, null, (searchQuery) => this.searchService.getResults(searchQuery, null));
     }
 }
